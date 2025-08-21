6ae39e6e8e2117db191118f1afbc870a856092a0
JuanEndl <juanendl-_@hotmail.com>
2025-08-21 20:40:27 -0300

    modificacion de la query 2

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ select
   concat(p.description, ' ', pp.peso) as descripcion,
   m.marca,
   a.animales,
-  concat('$',' ', p.precioCompra) as precioCompra,
+  concat('$',' ', ROUND (p.precioCompra)) as precioCompra,
   concat('$', ' ', ROUND (p.precioCompra * 1.25)) as precioVenta
 
 from productos p 
